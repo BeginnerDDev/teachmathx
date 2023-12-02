a094fe2881a5960f1e641ba9aacd4ee3910dc1d4
YAPVDev <vladimirpierra369@gmail.com>
2023-12-02 00:59:27 -0400

    Agregando animacion al menu

--- src/js/funciones.js
+++ src/js/funciones.js
@@ -1,85 +1,109 @@
-let questions = [
-    {
-        id: 1,
-        question: "Si \(f(x) = 2x + 3\) | ¿Cuál es el valor de \(f(5)\)",
-        answer: "b) 13",
-        options: [
-            "a) 8",
-            "b) 13",
-            "c) 15",
-            "d) 23"
-        ]
-    },
-    {
-        id: 2,
-        question: "Si \(g(x) = x^2 - 4\)",
-        answer: "",
-        options: [
+let questionsF = [
+	{
+		id: 1,
+        image: 'bg.webp',
+		question: ' ¿Cuál es el valor de \(f(5)\)?',
+		answer: 'b) 13',
+		options: ['a) 8', 'b) 13', 'c) 15', 'd) 23'],
+	},
+	{
+		id: 2,
+        image: 'bg.webp',
+		question: 'Si (f(x) = 2x + 3) | ¿Cuál es el valor de (f(6))',
+		answer: 'b) 13',
+		options: ['a) 8', 'b) 13', 'c) 15', 'd) 23'],
+	},
+	{
+		id: 3,
+        image: 'bg.webp',
+		question: 'Si (f(x) = 2x + 3) | ¿Cuál es el valor de (f(5))',
+		answer: 'b) 13',
+		options: ['a) 8', 'b) 13', 'c) 15', 'd) 23'],
+	},
+	{
+		id: 4,
+		question: 'Si (f(x) = 2x + 3) | ¿Cuál es el valor de (f(5))',
+		answer: 'b) 13',
+		options: ['a) 8', 'b) 13', 'c) 15', 'd) 23'],
+	},
+	{
+		id: 5,
+		question: 'Si (f(x) = 2x + 3) | ¿Cuál es el valor de (f(5))',
+		answer: 'b) 13',
+		options: ['a) 8', 'b) 13', 'c) 15', 'd) 23'],
+	},
+	{
+		id: 6,
+		question: 'Si (f(x) = 2x + 3) | ¿Cuál es el valor de (f(5))',
+		answer: 'b) 13',
+		options: ['a) 8', 'b) 13', 'c) 15', 'd) 23'],
+	},
+	{
+		id: 7,
+		question: 'Si (f(x) = 2x + 3) | ¿Cuál es el valor de (f(5))',
+		answer: 'b) 13',
+		options: ['a) 8', 'b) 13', 'c) 15', 'd) 23'],
+	},
+	{
+		id: 8,
+		question: 'Si (f(x) = 2x + 3) | ¿Cuál es el valor de (f(5))',
+		answer: 'b) 13',
+		options: ['a) 8', 'b) 13', 'c) 15', 'd) 23'],
+	},
+	{
+		id: 9,
+		question: 'Si (f(x) = 2x + 3) | ¿Cuál es el valor de (f(5))',
+		answer: 'b) 13',
+		options: ['a) 8', 'b) 13', 'c) 15', 'd) 23'],
+	},
+	{
+		id: 10,
+		question: 'Si (f(x) = 2x + 3) | ¿Cuál es el valor de (f(5))',
+		answer: 'b) 13',
+		options: ['a) 8', 'b) 13', 'c) 15', 'd) 23'],
+	},
+];
 
-        ]
-    },
-    {
-        id: 3,
-        question: "",
-        answer: "",
-        options: [
+const starBtn = document.querySelector('.home__btn');
+const popupInfo = document.querySelector('.popup');
+const exitBtn = document.querySelector('.exit-btn');
+const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continue-btn');
+const quizSection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
 
-        ]
-    },
-    {
-        id: 4,
-        question: "",
-        answer: "",
-        options: [
+starBtn.onclick = () => {
+	popupInfo.classList.add('active');
+	main.classList.add('active');
+};
 
-        ]
-    },
-    {
-        id: 5,
-        question: "",
-        answer: "",
-        options: [
+exitBtn.onclick = () => {
+	popupInfo.classList.remove('active');
+	main.classList.remove('active');
+};
 
-        ]
-    },
-    {
-        id: 6,
-        question: "",
-        answer: "",
-        options: [
+continueBtn.onclick = () => {
+	quizSection.classList.add('active');
+	popupInfo.classList.remove('active');
+	main.classList.remove('active');
+	quizBox.classList.add('active');
 
-        ]
-    },
-    {
-        id: 7,
-        question: "",
-        answer: "",
-        options: [
+	showQuestion(0);
+};
 
-        ]
-    },
-    {
-        id: 8,
-        question: "",
-        answer: "",
-        options: [
+let questionCount = 0;
 
-        ]
-    },
-    {
-        id: 9,
-        question: "",
-        answer: "",
-        options: [
+const nextBtn = document.querySelector('.next-btn');
 
-        ]
-    },
-    {
-        id: 10,
-        question: "",
-        answer: "",
-        options: [
+nextBtn.onclick = () => {
+	questionCount++;
+	showQuestion(questionCount);
+};
 
-        ]
-    },
-]
+function showQuestion(i) {
+	const questionText = document.querySelector('.question-text');
+	const questionImage = document.querySelector('.question-image');
+	
+    questionImage.src = question[i].image
+    questionText.textContent = `${questionsF[i].question}`;
+}
